f2ec1b519f5e58f20fe1c8b5713cd644466e1a07
simahojr <simeonpande@outlook.com>
2025-12-09 20:49:27 +0100

    templating

---
+++ PluginBuild/build.gradle.kts
@@ -0,0 +1,22 @@
+
+// Root build.gradle.kts
+plugins {
+    // Do not apply AGP here; apply it in the module
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.2")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
---
+++ PluginBuild/settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "Solar2D-TTLock-Plugin"
+
+include("app")
+//includeBuild("libs")
+
