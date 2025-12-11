b4b4d11401bc7b78af9660948c77e6f0da6489a0
simahojr <simeonpande@outlook.com>
2025-12-12 00:41:38 +0100

    added a build sys

---
+++ src/android/plugin/metadata.lua
@@ -0,0 +1,17 @@
+-- metadata.lua
+return {
+    plugin = {
+        format = "jar",        -- or "aar" if using an Android library
+        publisherId = "com.shannonit",
+        name = "plugin.ttlock",
+        version = "1.0.0",
+        minSdkVersion = 23,    -- minimum Android SDK
+        maxSdkVersion = 34,    -- optional
+        supportedPlatforms = {
+            android = true,
+            iphone = false      -- set true if you have iOS implementation
+        },
+        description = "TTLock Bluetooth lock integration for Solar2D",
+        license = "MIT",       -- optional
+    }
+}
