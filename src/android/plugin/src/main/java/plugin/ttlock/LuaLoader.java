62cae5c5c291ec1adba6bf06226d865392481f16
simahojr <simeonpande@outlook.com>
2025-12-12 08:28:04 +0100

    changed corona.graddle

--- src/android/plugin/src/main/java/plugin/ttlock/LuaLoader.java
+++ src/android/plugin/src/main/java/plugin/ttlock/LuaLoader.java
@@ -5,18 +5,18 @@ import android.content.Context;
 import com.ansca.corona.CoronaRuntime;
 import com.ansca.corona.CoronaRuntimeTaskDispatcher;
 
-public class LuaLoader {
+// public class LuaLoader {
 
-    private final LuaTTLock luaTTLock;
-    private final CoronaRuntimeTaskDispatcher dispatcher;
+//     private final LuaTTLock luaTTLock;
+//     private final CoronaRuntimeTaskDispatcher dispatcher;
 
-    public LuaLoader(Context context, CoronaRuntime runtime) {
-        dispatcher = new CoronaRuntimeTaskDispatcher(runtime);
+//     public LuaLoader(Context context, CoronaRuntime runtime) {
+//         dispatcher = new CoronaRuntimeTaskDispatcher(runtime);
 
-        // Create TTLock wrapper (minimal version)
-        luaTTLock = new LuaTTLock(context, dispatcher);
+//         // Create TTLock wrapper (minimal version)
+//         luaTTLock = new LuaTTLock(context, dispatcher);
 
-        // Optional test message
-        System.out.println("LuaLoader: LuaTTLock instance created");
-    }
-}
+//         // Optional test message
+//         System.out.println("LuaLoader: LuaTTLock instance created");
+//     }
+// }
--- src/android/plugin/src/main/java/plugin/ttlock/LuaTTLock.java
+++ src/android/plugin/src/main/java/plugin/ttlock/LuaTTLock.java
@@ -15,99 +15,99 @@ import com.ttlock.bl.sdk.constant.ControlAction;
 import com.ttlock.bl.sdk.entity.ControlLockResult;
 import com.ttlock.bl.sdk.entity.LockError;
 
-public class LuaTTLock {
+// public class LuaTTLock {
 
-    private final Context context;
-    private final TTLockClient ttlock;
-    private final CoronaRuntimeTaskDispatcher dispatcher;
+//     private final Context context;
+//     private final TTLockClient ttlock;
+//     private final CoronaRuntimeTaskDispatcher dispatcher;
 
-    public LuaTTLock(Context context, CoronaRuntimeTaskDispatcher dispatcher) {
-        this.context = context;
-        this.dispatcher = dispatcher;
-        ttlock = TTLockClient.getDefault();
-        ttlock.prepareBTService(context);
-    }
+//     public LuaTTLock(Context context, CoronaRuntimeTaskDispatcher dispatcher) {
+//         this.context = context;
+//         this.dispatcher = dispatcher;
+//         ttlock = TTLockClient.getDefault();
+//         ttlock.prepareBTService(context);
+//     }
 
-    public void startScan() {
-        ttlock.startScanLock(new ScanLockCallback() {
-            @Override
-            public void onScanLockSuccess(ExtendedBluetoothDevice device) {
-                final String name = device.getName();
-                final String mac = device.getAddress();
+//     public void startScan() {
+//         ttlock.startScanLock(new ScanLockCallback() {
+//             @Override
+//             public void onScanLockSuccess(ExtendedBluetoothDevice device) {
+//                 final String name = device.getName();
+//                 final String mac = device.getAddress();
 
-                dispatcher.send(new CoronaRuntimeTask() {
-                    @Override
-                    public void executeUsing(CoronaRuntime runtime) {
-                        System.out.println("TTLock Scan: " + name + " / " + mac);
-                    }
-                });
-            }
+//                 dispatcher.send(new CoronaRuntimeTask() {
+//                     @Override
+//                     public void executeUsing(CoronaRuntime runtime) {
+//                         System.out.println("TTLock Scan: " + name + " / " + mac);
+//                     }
+//                 });
+//             }
 
-            @Override
-            public void onFail(LockError error) {
-                dispatcher.send(new CoronaRuntimeTask() {
-                    @Override
-                    public void executeUsing(CoronaRuntime runtime) {
-                        System.err.println("TTLock Scan failed: " + error.getErrorMsg());
-                    }
-                });
-            }
-        });
-    }
+//             @Override
+//             public void onFail(LockError error) {
+//                 dispatcher.send(new CoronaRuntimeTask() {
+//                     @Override
+//                     public void executeUsing(CoronaRuntime runtime) {
+//                         System.err.println("TTLock Scan failed: " + error.getErrorMsg());
+//                     }
+//                 });
+//             }
+//         });
+//     }
 
-    public void stopScan() {
-        ttlock.stopScanLock();
-    }
+//     public void stopScan() {
+//         ttlock.stopScanLock();
+//     }
 
-    public void initLock(ExtendedBluetoothDevice device) {
-        ttlock.initLock(device, new InitLockCallback() {
-            @Override
-            public void onInitLockSuccess(String lockData) {
-                dispatcher.send(new CoronaRuntimeTask() {
-                    @Override
-                    public void executeUsing(CoronaRuntime runtime) {
-                        System.out.println("TTLock init success: " + lockData);
-                    }
-                });
-            }
+//     public void initLock(ExtendedBluetoothDevice device) {
+//         ttlock.initLock(device, new InitLockCallback() {
+//             @Override
+//             public void onInitLockSuccess(String lockData) {
+//                 dispatcher.send(new CoronaRuntimeTask() {
+//                     @Override
+//                     public void executeUsing(CoronaRuntime runtime) {
+//                         System.out.println("TTLock init success: " + lockData);
+//                     }
+//                 });
+//             }
 
-            @Override
-            public void onFail(LockError error) {
-                dispatcher.send(new CoronaRuntimeTask() {
-                    @Override
-                    public void executeUsing(CoronaRuntime runtime) {
-                        System.err.println("TTLock init failed: " + error.getErrorMsg());
-                    }
-                });
-            }
-        });
-    }
+//             @Override
+//             public void onFail(LockError error) {
+//                 dispatcher.send(new CoronaRuntimeTask() {
+//                     @Override
+//                     public void executeUsing(CoronaRuntime runtime) {
+//                         System.err.println("TTLock init failed: " + error.getErrorMsg());
+//                     }
+//                 });
+//             }
+//         });
+//     }
 
-    public void unlock(String lockData, String lockMac) {
-        ttlock.controlLock(ControlAction.UNLOCK, lockData, lockMac, new ControlLockCallback() {
-            @Override
-            public void onControlLockSuccess(ControlLockResult result) {
-                dispatcher.send(new CoronaRuntimeTask() {
-                    @Override
-                    public void executeUsing(CoronaRuntime runtime) {
-                        System.out.println("TTLock unlocked. Battery: " + result.getBattery());
-                    }
-                });
-            }
+//     public void unlock(String lockData, String lockMac) {
+//         ttlock.controlLock(ControlAction.UNLOCK, lockData, lockMac, new ControlLockCallback() {
+//             @Override
+//             public void onControlLockSuccess(ControlLockResult result) {
+//                 dispatcher.send(new CoronaRuntimeTask() {
+//                     @Override
+//                     public void executeUsing(CoronaRuntime runtime) {
+//                         System.out.println("TTLock unlocked. Battery: " + result.getBattery());
+//                     }
+//                 });
+//             }
 
-            @Override
-            public void onFail(LockError error) {
-                dispatcher.send(new CoronaRuntimeTask() {
-                    @Override
-                    public void executeUsing(CoronaRuntime runtime) {
-                        System.err.println("TTLock unlock failed: " + error.getErrorMsg());
-                    }
-                });
-            }
-        });
-    }
+//             @Override
+//             public void onFail(LockError error) {
+//                 dispatcher.send(new CoronaRuntimeTask() {
+//                     @Override
+//                     public void executeUsing(CoronaRuntime runtime) {
+//                         System.err.println("TTLock unlock failed: " + error.getErrorMsg());
+//                     }
+//                 });
+//             }
+//         });
+//     }
 
-    public void release() {
-        ttlock.stopBTService();
-    }
-}
+//     public void release() {
+//         ttlock.stopBTService();
+//     }
+// }
