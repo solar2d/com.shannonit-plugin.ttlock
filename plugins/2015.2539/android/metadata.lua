c5bb164e6b28299f75736a293ab878761bbed5d3
simahojr <simeonpande@outlook.com>
2025-12-12 08:41:11 +0100

    changed corona.graddle

--- plugins/2015.2539/android/metadata.lua
+++ plugins/2015.2539/android/metadata.lua
@@ -1,16 +1,12 @@
 -- metadata.lua
 return {
     plugin = {
-        format = "aar",        -- or "aar" if using an Android library
+        format = "jar",        -- or "aar" if using an Android library
         publisherId = "com.shannonit",
         name = "plugin.ttlock",
         version = "1.0.0",
         minSdkVersion = 23,    -- minimum Android SDK
         maxSdkVersion = 34,    -- optional
-        supportedPlatforms = {
-            android = true,
-            iphone = false      -- set true if you have iOS implementation
-        },
         description = "TTLock Bluetooth lock integration for Solar2D",
         license = "MIT",       -- optional
     }
