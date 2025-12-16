a9d490c6c44dca519afa63ff33d8bde505053d3f
simahojr <simeonpande@outlook.com>
2025-12-16 23:30:18 +0100

    deleted the data folder that wes wrongly inserted

--- src/android/build.gradle.kts
+++ src/android/build.gradle.kts
@@ -5,9 +5,10 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        //classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
         classpath("com.android.tools.build:gradle:7.4.2")
-        //classpath("com.beust:klaxon:5.5")
+        classpath("com.github.dcendents:android-maven-gradle-plugin:1.5")
+        classpath("com.beust:klaxon:5.5")
     }
 }
 
