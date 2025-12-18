8534101dcc39b9ac4009837855ae3140c08b2285
simahojr <simeonpande@outlook.com>
2025-12-18 15:02:57 +0100

    deleted the data folder that wes wrongly inserted

--- src/android/plugin/src/main/java/plugin/ttlock/LuaLoader.java
+++ src/android/plugin/src/main/java/plugin/ttlock/LuaLoader.java
@@ -1,34 +1,24 @@
 package plugin.ttlock;
 
-import android.content.Context;
-
-import com.ansca.corona.CoronaRuntime;
-import com.ansca.corona.CoronaRuntimeTaskDispatcher;
-
-import com.ansca.corona.CoronaActivity;
 import com.ansca.corona.CoronaEnvironment;
-import com.ansca.corona.CoronaLua;
-import com.ansca.corona.CoronaRuntime;
 import com.ansca.corona.CoronaRuntimeListener;
-import com.ansca.corona.CoronaRuntimeTask;
-import com.ansca.corona.permissions.PermissionState;
-import com.ansca.corona.permissions.PermissionsServices;
-import com.naef.jnlua.JavaFunction;
 import com.naef.jnlua.LuaState;
-import com.naef.jnlua.NamedJavaFunction;
 
 public class LuaLoader {
 
-    private final LuaTTLock luaTTLock;
+    // REQUIRED entry point
+    public static int luaopen_plugin_ttlock(LuaState L) {
 
-    public LuaLoader(Context context, CoronaRuntime runtime) {
-        CoronaRuntimeTaskDispatcher dispatcher =
-                new CoronaRuntimeTaskDispatcher(runtime);
+        // Register runtime listener ONCE
+        CoronaEnvironment.addRuntimeListener(new LuaTTLock());
 
-        // Create the LuaTTLock stub instance
-        luaTTLock = new LuaTTLock(dispatcher);
+        // Create Lua table
+        L.newTable();
 
-        // Log to confirm the loader is invoked
-        System.out.println("LuaLoader: LuaTTLock stub loaded");
+        // Expose functions
+        L.pushJavaFunction(new LuaTTLock.PingFunction());
+        L.setField(-2, "ping");
+
+        return 1;
     }
 }
--- src/android/plugin/src/main/java/plugin/ttlock/LuaTTLock.java
+++ src/android/plugin/src/main/java/plugin/ttlock/LuaTTLock.java
@@ -1,44 +1,58 @@
 package plugin.ttlock;
 
 import com.ansca.corona.CoronaRuntime;
-import com.ansca.corona.CoronaRuntimeTask;
-import com.ansca.corona.CoronaRuntimeTaskDispatcher;
-
-import com.ansca.corona.CoronaActivity;
-import com.ansca.corona.CoronaEnvironment;
-import com.ansca.corona.CoronaLua;
-import com.ansca.corona.CoronaRuntime;
 import com.ansca.corona.CoronaRuntimeListener;
 import com.ansca.corona.CoronaRuntimeTask;
-import com.ansca.corona.permissions.PermissionState;
-import com.ansca.corona.permissions.PermissionsServices;
+import com.ansca.corona.CoronaRuntimeTaskDispatcher;
 import com.naef.jnlua.JavaFunction;
 import com.naef.jnlua.LuaState;
-import com.naef.jnlua.NamedJavaFunction;
 
-public class LuaTTLock {
+public class LuaTTLock implements CoronaRuntimeListener {
 
-    private final CoronaRuntimeTaskDispatcher dispatcher;
+    // Cached runtime (set by Corona)
+    private static CoronaRuntime runtime;
+    private static CoronaRuntimeTaskDispatcher dispatcher;
 
-    public LuaTTLock(CoronaRuntimeTaskDispatcher dispatcher) {
-        this.dispatcher = dispatcher;
-
-        // Plugin initialization log
-        dispatcher.send(new CoronaRuntimeTask() {
-            @Override
-            public void executeUsing(CoronaRuntime runtime) {
-                System.out.println("LuaTTLock: Plugin successfully initialized (BLE-free stub)");
-            }
-        });
+    // Called by Corona when runtime starts
+    @Override
+    public void onLoaded(CoronaRuntime coronaRuntime) {
+        runtime = coronaRuntime;
+        dispatcher = new CoronaRuntimeTaskDispatcher(runtime);
+        System.out.println("LuaTTLock: runtime loaded");
     }
 
-    // Example test method callable from Lua
-    public void ping() {
-        dispatcher.send(new CoronaRuntimeTask() {
-            @Override
-            public void executeUsing(CoronaRuntime runtime) {
-                System.out.println("LuaTTLock: ping() called from Lua");
+    @Override
+    public void onStarted(CoronaRuntime runtime) {}
+
+    @Override
+    public void onSuspended(CoronaRuntime runtime) {}
+
+    @Override
+    public void onResumed(CoronaRuntime runtime) {}
+
+    @Override
+    public void onExiting(CoronaRuntime runtime) {
+        LuaTTLock.runtime = null;
+        dispatcher = null;
+    }
+
+    // ---- Lua function ----
+    public static class PingFunction implements JavaFunction {
+        @Override
+        public int invoke(LuaState L) {
+            if (dispatcher == null) {
+                System.out.println("LuaTTLock: runtime not ready");
+                return 0;
             }
-        });
+
+            dispatcher.send(new CoronaRuntimeTask() {
+                @Override
+                public void executeUsing(CoronaRuntime runtime) {
+                    System.out.println("LuaTTLock: ping() called from Lua");
+                }
+            });
+
+            return 0;
+        }
     }
 }
